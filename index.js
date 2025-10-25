19edd23097555cf4f798c017b606b4070d7b29cf
TomoyoDV <artemandrievich23@gmail.com>
2025-10-25 14:02:12 +0300

    Add branch(add html scrtucture, add js create element fucn)

---
+++ index.js
@@ -0,0 +1,18 @@
+function createElement(tag, props = {}, ...children) {
+  const el = document.createElement(tag);
+
+  for (const key in props) {
+    if (key.startsWith("on") && typeof props[key] === "function") {
+      el.addEventListener(key.substring(2).toLowerCase(), props[key]);
+    } else {
+      el.setAttribute(key, props[key]);
+    }
+  }
+
+  children.flat().forEach(child => {
+    if (typeof child === "string") el.appendChild(document.createTextNode(child));
+    else if (child instanceof Node) el.appendChild(child);
+  });
+
+  return el;
+}
